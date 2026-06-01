53576f01b67124b42f1fb6310e476a450e14c236
DeepshikhaDey28 <dey.deepshikha.283@gmail.com>
2026-06-01 20:52:41 +0530

    This is a commit on the topic "Type Casting"

---
+++ TypeCasting.py
@@ -0,0 +1,19 @@
+#converting onne data type to another
+
+name = "Deepshikha"
+age = 21
+cgpa = 8.9
+is_student = True
+
+print(type(name))
+cgpa = int(cgpa)
+print(cgpa)
+age = float(age)
+print(age)
+age = str(age)
+# age = age + 1 will not work without quotes
+age = "age + 1"
+print (age)
+name = bool(name)
+print(name)
+
